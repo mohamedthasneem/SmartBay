acb1362ed77fb23ff1cbe2d2ce353fab8822f915
Aashik Ahamed <aashik.ahamed12@gmail.com>
2019-03-29 16:37:40 +0530

    Project Created

---
+++ smartbay/src/main/java/com/uok/smartbay/dev/smartbay/SmartbayApplication.java
@@ -0,0 +1,13 @@
+package com.uok.smartbay.dev.smartbay;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SmartbayApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SmartbayApplication.class, args);
+	}
+
+}
