d29f8e5f1ffbaa33ff628ec3a74b11fd346d1bf7
mohamedthasneem <amythasneem@gmail.com>
2019-03-30 14:54:17 -0700

    Rearrange files

---
+++ smartbay/backend/src/test/java/com/uok/smartbay/dev/smartbay/SmartbayApplicationTests.java
@@ -0,0 +1,16 @@
+package com.uok.smartbay.dev.smartbay;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class SmartbayApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
