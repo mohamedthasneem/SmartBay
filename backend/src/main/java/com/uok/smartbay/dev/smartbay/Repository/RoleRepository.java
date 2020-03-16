bcddecd38a25010e9c65424b3a0f80bf0f793d87
Aashik Ahamed <aashik.ahamed12@gmail.com>
2020-03-16 14:10:43 +0530

    Add products store - changes added

--- backend/src/main/java/com/uok/smartbay/dev/smartbay/Repository/RoleRepository.java
+++ backend/src/main/java/com/uok/smartbay/dev/smartbay/Repository/RoleRepository.java
@@ -3,6 +3,6 @@ package com.uok.smartbay.dev.smartbay.Repository;
 import com.uok.smartbay.dev.smartbay.Model.Role;
 import org.springframework.data.mongodb.repository.MongoRepository;
 
-public interface RoleRepository extends MongoRepository<Role,String>{
+public interface  RoleRepository extends MongoRepository<Role,String>{
     Role findByRole(String role);
 }
