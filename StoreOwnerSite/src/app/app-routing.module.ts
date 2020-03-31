5338d14cd486f63213827edcb90f628b8f3f7c1b
Aashik Ahamed <aashik.ahamed12@gmail.com>
2020-03-31 23:08:54 +0530

    Store owner home and dashboard components added

--- StoreOwnerSite/src/app/app-routing.module.ts
+++ StoreOwnerSite/src/app/app-routing.module.ts
@@ -2,10 +2,14 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { RegisterComponent } from './components/register/register.component';
 import { LoginComponent } from './components/login/login.component';
+import { HomeComponent } from './components/home/home.component';
+import { DashboardComponent } from './components/dashboard/dashboard.component';
 
 const routes: Routes = [
   {path:'register',component: RegisterComponent},
-  {path:'login',component:LoginComponent}
+  {path:'login',component:LoginComponent},
+  {path:'home',component: HomeComponent},
+  {path:'dashboard',component:DashboardComponent}
 ];
 
 @NgModule({
--- StoreOwnerSite/src/app/app.module.ts
+++ StoreOwnerSite/src/app/app.module.ts
@@ -5,12 +5,16 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { RegisterComponent } from './components/register/register.component';
 import { LoginComponent } from './components/login/login.component';
+import { HomeComponent } from './components/home/home.component';
+import { DashboardComponent } from './components/dashboard/dashboard.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     RegisterComponent,
-    LoginComponent
+    LoginComponent,
+    HomeComponent,
+    DashboardComponent
   ],
   imports: [
     BrowserModule,
