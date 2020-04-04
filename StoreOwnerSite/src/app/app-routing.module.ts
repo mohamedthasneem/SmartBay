6f9a8a93409ff0bf24d2ee236cf540ccd2ee5a27
Aashik Ahamed <aashik.ahamed12@gmail.com>
2020-04-04 18:22:20 +0530

    store owner dashboard components added

--- StoreOwnerSite/src/app/app-routing.module.ts
+++ StoreOwnerSite/src/app/app-routing.module.ts
@@ -4,12 +4,38 @@ import { RegisterComponent } from './components/register/register.component';
 import { LoginComponent } from './components/login/login.component';
 import { HomeComponent } from './components/home/home.component';
 import { DashboardComponent } from './components/dashboard/dashboard.component';
+import { ProductComponent } from './components/dashboard/product/product.component';
+import { OrderComponent } from './components/dashboard/order/order.component';
+import { CustomerComponent } from './components/dashboard/customer/customer.component';
+import { AddProductComponent } from './components/dashboard/product/add-product/add-product.component';
+import { ViewProductComponent } from './components/dashboard/product/view-product/view-product.component';
+import { ViewOrderComponent } from './components/dashboard/order/view-order/view-order.component';
+import { ViewCustomerComponent } from './components/dashboard/customer/view-customer/view-customer.component';
 
 const routes: Routes = [
   {path:'register',component: RegisterComponent},
   {path:'login',component:LoginComponent},
   {path:'home',component: HomeComponent},
-  {path:'dashboard',component:DashboardComponent}
+  {path:'dashboard',component:DashboardComponent,
+   children : [
+     {path:'products',component:ProductComponent,
+      children : [
+        {path:'add',component:AddProductComponent},
+        {path:'view',component:ViewProductComponent}
+      ]
+      },
+     {path:'orders',component:OrderComponent,
+      children : [
+        {path:'view',component:ViewOrderComponent}
+      ]
+      },
+     {path:'customers',component:CustomerComponent,
+      children : [
+        {path:'view',component:ViewCustomerComponent}
+      ]
+      }
+   ] 
+  }
 ];
 
 @NgModule({
--- StoreOwnerSite/src/app/app.module.ts
+++ StoreOwnerSite/src/app/app.module.ts
@@ -7,6 +7,13 @@ import { RegisterComponent } from './components/register/register.component';
 import { LoginComponent } from './components/login/login.component';
 import { HomeComponent } from './components/home/home.component';
 import { DashboardComponent } from './components/dashboard/dashboard.component';
+import { ProductComponent } from './components/dashboard/product/product.component';
+import { OrderComponent } from './components/dashboard/order/order.component';
+import { CustomerComponent } from './components/dashboard/customer/customer.component';
+import { AddProductComponent } from './components/dashboard/product/add-product/add-product.component';
+import { ViewProductComponent } from './components/dashboard/product/view-product/view-product.component';
+import { ViewCustomerComponent } from './components/dashboard/customer/view-customer/view-customer.component';
+import { ViewOrderComponent } from './components/dashboard/order/view-order/view-order.component';
 
 @NgModule({
   declarations: [
@@ -14,7 +21,14 @@ import { DashboardComponent } from './components/dashboard/dashboard.component';
     RegisterComponent,
     LoginComponent,
     HomeComponent,
-    DashboardComponent
+    DashboardComponent,
+    ProductComponent,
+    OrderComponent,
+    CustomerComponent,
+    AddProductComponent,
+    ViewProductComponent,
+    ViewCustomerComponent,
+    ViewOrderComponent
   ],
   imports: [
     BrowserModule,
