6c9a55c8221e9eb5f3f0c1814ed69769ee72ebef
Aashik96 <aashik.ahamed12@gmail.com>
2020-06-29 09:14:25 -0700

    Payment gateway intergrated

--- Smartbay_Latest/CustomerSite/src/app/components/checkout/checkout.component.ts
+++ Smartbay_Latest/CustomerSite/src/app/components/checkout/checkout.component.ts
@@ -1,16 +1,21 @@
-import { Component, OnInit, Input } from '@angular/core';
+import { Component, OnInit, Input,AfterViewChecked} from '@angular/core';
 import { Product } from 'src/app/models/product';
 import { CustomerService } from 'src/app/services/customer.service';
+
+declare let paypal: any;
 
 @Component({
   selector: 'app-checkout',
   templateUrl: './checkout.component.html',
   styleUrls: ['./checkout.component.css']
 })
-export class CheckoutComponent implements OnInit {
+export class CheckoutComponent implements OnInit,AfterViewChecked {
 
   cartTotal : number;
   cartItems : Product[];
+
+  addScript: boolean = false;
+  paypalLoad: boolean = true;
 
   @Input()
   order = {
@@ -34,6 +39,48 @@ export class CheckoutComponent implements OnInit {
     this.customerService.createOrder(this.order).subscribe(data =>{
       data = this.order;
     });  
+  }
+
+  paypalConfig = {
+    env: 'sandbox',
+    client: {
+      sandbox: 'AX8hRFUDYOLdljadiSqrSx7Ds6phxIZAIAqnW8FXfXCbXBS1fomajvcNz1esrLfSZo1jkrcoLajzFPDc'
+    },
+    commit: true,
+    payment: (data, actions) => {
+      return actions.payment.create({
+        payment: {
+          transactions: [
+            { amount: { total: this.cartTotal, currency: 'USD' } }
+          ]
+        }
+      });
+    },
+    onAuthorize: (data, actions) => {
+      return actions.payment.execute().then((payment) => {
+        alert('Transaction successfull,Click place your order');  
+        //Do something when payment is successful.
+      })
+    }
+  };
+ 
+  ngAfterViewChecked(): void {
+    if (!this.addScript) {
+      this.addPaypalScript().then(() => {
+        paypal.Button.render(this.paypalConfig, '#paypal-checkout-btn');
+        this.paypalLoad = false;
+      })
+    }
+  }
+  
+  addPaypalScript() {
+    this.addScript = true;
+    return new Promise((resolve, reject) => {
+      let scripttagElement = document.createElement('script');    
+      scripttagElement.src = 'https://www.paypalobjects.com/api/checkout.js';
+      scripttagElement.onload = resolve;
+      document.body.appendChild(scripttagElement);
+    })
   }
 
 }
